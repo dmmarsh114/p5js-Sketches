0a3e8aca7fcf4c01ff305b68de6090921684c271
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-06-01 18:12:01 -0400

    modifying mandala code

--- mandala/sketch.js
+++ mandala/sketch.js
@@ -1,65 +1,22 @@
-let radius;
-let count;
-let a = 0;
-let rate = 1;
-let mainRate = .25;
-
-let mandalaDesigns = [];
-
 function setup() {
     let canvas = createCanvas(500, 500);
     canvas.parent(document.getElementById('artContainer'));
 
-    radius = random(40, 80);
-    count = Math.floor(random(4, 8));
+    colorMode(HSB);
 }
 
 function draw() {
-    background(0);
-
-    stroke(255);
-    noFill();
-    ellipseMode(CENTER);
-
+    background(57, 18, 100);
     translate(width / 2, height / 2);
+    noFill();
+    stroke('black');
+    ellipse(0, 0, 300);
+    ellipse(0, 0, 150);
 
-    ellipse(0, 0, radius * 2);
-
-    // draw lines from 0,0 to radius
-    push();
-    for (let i = 0; i <= ((count * 2) - 1); i++) {
-        rotate(PI / count);
-        line(0, 0, 0, radius);
-    }
-    pop();
-
-    stroke('red');
-    push();
-    for (let i = 0; i <= (((count / 2) * 2) - 1); i++) {
-        rotate(PI / (count / 2));
-        line(0, 0, 0, radius);
-    }
-    pop();
-
-    stroke('blue');
-    ellipse(0, 0, a);
-    ellipse(0, 0, a + count);
-
-    radius += mainRate;
-    count += .05;
-    a += mainRate;
-
-    if (a >= radius * 2) {
-        rate = -1;
-    }
-
-    if (radius >= 300) {
-        mainRate = -.25;
-        count = 0;
-        rate = -1;
-    }
-
-    if (radius <= 10) {
-        mainRate = .25;
+    for (let i = 0; i < 15; i++) {
+        rotate(PI / 14);
+        noStroke();
+        fill(127, 100, 100, 0.5)
+        ellipse(0, 0, 300, 20);
     }
 }
