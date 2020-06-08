71be0d1eb62712235893f403ea753c994c9b107d
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-06-07 22:33:10 -0400

    finished perlin noise flowfield sketch

--- perlin noise/sketch.js
+++ perlin noise/sketch.js
@@ -7,42 +7,59 @@ let cols, rows;
 let zoff = 0;
 let inc = 0.1;
 
+let particleCount = 200;
+let particles = [];
+let flowfield = [];
+
 let fr;
 
 function setup() {
-    let canvas = createCanvas(400, 400);
+    let canvas = createCanvas(800, 450);
+    // let canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent(document.getElementById('artContainer'));
+
+    colorMode(HSB);
+    background('black');
 
     cols = floor(width / scale);
     rows = floor(height / scale);
 
+    for (let i = 0; i < particleCount; i++) {
+        let newParticle = new Particle();
+        particles.push(newParticle);
+    }
+
+    // flowfield = new Array(rows * cols);
+
     // create a p tag that will print the current framerate (for monitoring)
-    fr = createP('FrameRate');
-    fr.parent(document.getElementById('parent'));
+    // fr = createP('FrameRate');
+    // fr.parent(document.getElementById('parent'));
 }
 
 function draw() {
-    background(0);
 
     let yoff = 0;
-    for (let y = 0; y < height; y += scale) {
+    for (let y = 0; y < rows; y++) {
         let xoff = 0;
-        for (let x = 0; x < width; x += scale) {
-            let angle = noise(xoff, yoff, zoff) * TWO_PI;
+        for (let x = 0; x < cols; x++) {
+            let index = x + y * cols;
+            let angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
             let v = p5.Vector.fromAngle(angle);
+            v.setMag(1);
+            flowfield[index] = v;
             xoff += inc;
-
-            stroke('white');
-            push();
-            translate(x, y);
-            rotate(v.heading());
-            line(0, 0, scale, 0);
-            pop();
-
         }
+
         yoff += inc;
-        zoff += 0.0005;
+        zoff += 0.0003;
     }
 
-    fr.html(floor(frameRate()));
+    particles.forEach(particle => {
+        particle.follow(flowfield);
+        particle.update();
+        particle.edges();
+        particle.show();
+    })
+
+    // fr.html(floor(frameRate()));
 }
--- tiles3/sketch.js
+++ tiles3/sketch.js
@@ -1,26 +1,13 @@
-let tileSize = 50;
-
 function setup() {
     let canvas = createCanvas(800, 450);
     canvas.parent(document.getElementById('artContainer'));
 
     colorMode(HSB);
-    // rectMode(CENTER);
+    rectMode(CENTER);
     noFill();
     background(57.4, 18.4, 100);
 }
 
 function draw() {
 
-    for (let i = 0; i < width; i += tileSize) {
-        for (let j = 0; j < height; j += tileSize) {
-            push();
-            // translate(i + (tileSize / 2), j + (tileSize / 2));
-            translate(i, j);
-            rect(0, 0, tileSize);
-            pop();
-        }
-    }
-
-    noLoop();
 }
